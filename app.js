928cb1adb9741fc3808cdeedf5e4d0e1a0826f73
Manuel Bauer <manuel.c.bauer@googlemail.com>
2021-10-11 19:21:41 -0500

    built basic data structure and add first functions

--- app.js
+++ app.js
@@ -1,1 +1,120 @@
-$("h1").css("color", "black");
+// Data structure
+
+let cardDeck = [
+  { name: "2_of_clubs", value: 2, type: "number" },
+  { name: "2_of_diamodns", value: 2, type: "number" },
+  { name: "2_of_hearts", value: 2, type: "number" },
+  { name: "2_of_spades", value: 2, type: "number" },
+  { name: "3_of_clubs", value: 3, type: "number" },
+  { name: "3_of_diamonds", value: 3, type: "number" },
+  { name: "3_of_hearts", value: 3, type: "number" },
+  { name: "3_of_spades", value: 3, type: "number" },
+  { name: "4_of_clubs", value: 4, type: "number" },
+  { name: "4_of_diamonds", value: 4, type: "number" },
+  { name: "4_of_hearts", value: 4, type: "number" },
+  { name: "4_of_spades", value: 4, type: "number" },
+  { name: "5_of_clubs", value: 5, type: "number" },
+  { name: "5_of_diamonds", value: 5, type: "number" },
+  { name: "5_of_hearts", value: 5, type: "number" },
+  { name: "5_of_spades", value: 5, type: "number" },
+  { name: "6_of_clubs", value: 6, type: "number" },
+  { name: "6_of_diamonds", value: 6, type: "number" },
+  { name: "6_of_hearts", value: 6, type: "number" },
+  { name: "6_of_spades", value: 6, type: "number" },
+  { name: "7_of_clubs", value: 7, type: "number" },
+  { name: "7_of_diamonds", value: 7, type: "number" },
+  { name: "7_of_hearts", value: 7, type: "number" },
+  { name: "7_of_spades", value: 7, type: "number" },
+  { name: "8_of_clubs", value: 8, type: "number" },
+  { name: "8_of_diamonds", value: 8, type: "number" },
+  { name: "8_of_hearts", value: 8, type: "number" },
+  { name: "8_of_spades", value: 8, type: "number" },
+  { name: "9_of_diamonds", value: 9, type: "number" },
+  { name: "9_of_hearts", value: 9, type: "number" },
+  { name: "9_of_spades", value: 9, type: "number" },
+  { name: "9_of_clubs", value: 10, type: "number" },
+  { name: "10_of_diamonds", value: 10, type: "number" },
+  { name: "10_of_hearts", value: 10, type: "number" },
+  { name: "10_of_spades", value: 10, type: "number" },
+  { name: "10_of_clubs", value: 10, type: "number" },
+  { name: "jack_of_diamonds", value: 10, type: "face" },
+  { name: "jack_of_hearts", value: 10, type: "face" },
+  { name: "jack_of_spades", value: 10, type: "face" },
+  { name: "jack_of_clubs", value: 10, type: "face" },
+  { name: "queen_of_diamonds", value: 10, type: "face" },
+  { name: "queen_of_hearts", value: 10, type: "face" },
+  { name: "queen_of_spades", value: 10, type: "face" },
+  { name: "queen_of_clubs", value: 10, type: "face" },
+  { name: "king_of_diamonds", value: 10, type: "face" },
+  { name: "king_of_hearts", value: 10, type: "face" },
+  { name: "king_of_spades", value: 10, type: "face" },
+  { name: "king_of_clubs", value: 10, type: "face" },
+  { name: "ace_of_diamonds", value: 11, type: "ace" },
+  { name: "ace_of_hearts", value: 11, type: "ace" },
+  { name: "ace_of_spades", value: 11, type: "ace" },
+  { name: "ace_of_clubs", value: 11, type: "ace" },
+];
+
+let player = {
+  hand: [],
+  handValue: 0,
+  totalScore: 0,
+  turn: true,
+};
+
+let dealer = {
+  hand: [],
+  handValue: 0,
+  totalScore: 0,
+  turn: false,
+};
+
+// Function that shuffles random card out of deck and returns it
+
+const shuffle = (deck) => {
+  let max = deck.length - 1;
+  let random = Math.floor(Math.random() * max);
+  return deck.splice(random, 1);
+};
+
+// Function that displays card
+
+const display = (player, card) => {
+  let path = `img\\deck\\${card.name}`;
+};
+
+// Init function
+
+const init = () => {
+  // Restore card deck
+  player.hand.forEach((card) => cardDeck.push(card));
+  dealer.hand.forEach((card) => cardDeck.push(card));
+  // Set Hand Values to Zero
+  $("p#player-hand-value").text(0);
+  $("p#dealer-hand-value").text(0);
+  // Removes Cards
+
+  // Pauses for 2 seconds
+
+  // Sets message "Starting Hands are Dealt"
+  $("#message").text("Starting hands are dealt");
+  // Adds First two player cards
+  player.hand.push(shuffle(cardDeck));
+  player.hand.push(shuffle(cardDeck));
+  // Show Cards
+
+  // Evaluate player score
+
+  // Adds First two dealer cards - one hidden, one open
+
+  // Evaluates Player score
+};
+
+// Function that evaluates Score
+
+// Event Handler
+
+$("#new-game").click(() => {
+  console.log("new game");
+  init();
+});
