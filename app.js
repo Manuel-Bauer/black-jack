a9ccbf189bb76f825984ef7416da4d2e626d0b9f
Manuel Bauer <manuel.c.bauer@googlemail.com>
2021-11-19 07:48:52 -0600

    delete static code

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-// Add serve
-
-const handler = require('serve-handler');
-const http = require('http');
-
-// You pass two more arguments for config and middleware
-// More details here: https://github.com/vercel/serve-handler#options
-const server = http.createServer((request, response) =>
-  handler(request, response)
-);
-
-server.listen(3000, () => {
-  console.log('Running at http://localhost:3000');
-});
-
 // DOM items
 const message = $('#message');
 const userCards = $('#user-cards');
